24ece1984a67b32cdee53f919b77e54be596e17c
MetaGuildX <metaguildx16@gmail.com>
2026-07-05 14:48:35 +0530

    fix: route all opBNB chains through Owlto adapter, prevent LI.FI token deny list error

--- apps/web/src/lib/bridge/registry.ts
+++ apps/web/src/lib/bridge/registry.ts
@@ -5,8 +5,14 @@ import { OwltoAdapter } from "./adapters/owlto";
 const lifiAdapter = new LiFiAdapter();
 const owltoAdapter = new OwltoAdapter();
 
+const OPBNB_CHAIN_ID = 204;
+
 export function getProvider(fromChainId: number, toChainId: number): IBridgeProvider {
   if (owltoAdapter.supportsRoute(fromChainId, toChainId)) {
+    return owltoAdapter;
+  }
+  // opBNB routes not supported by LI.FI - return owlto which will show no route
+  if (fromChainId === OPBNB_CHAIN_ID || toChainId === OPBNB_CHAIN_ID) {
     return owltoAdapter;
   }
   return lifiAdapter;
